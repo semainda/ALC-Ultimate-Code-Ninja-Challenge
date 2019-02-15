bdd7d6c6c629e85f6bf3c5e59cd0e0d042bca4c2
semainda <semaindas@gmail.com>
2019-02-16 01:46:30 +0300

    Update guessing-game.js with node-console.input module

--- challenge-2/guessing-game.js
+++ challenge-2/guessing-game.js
@@ -13,58 +13,45 @@
  * Author: semaindas@gmail.com 
  */
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const input = require('node-console-input');
 
 const chalk = require('chalk');
 
 let randomGeneratedNumber = Math.floor(Math.random() * 10) + 1;
 
 const guessingOrder = {1: 'first', 2: 'second', 3: 'third', 4: 'fourth', 5: 'fifth', 6: 'sixth', 7: 'seventh', 8: 'eighth'};
-let guessPosition = 1;
-
-const recursiveReadLine = () => {
-    readline.question(`\nWhat's your ${guessingOrder[guessPosition]} guess?: `,(guessedNumber) => {
-        guessPosition +=1;
-        if (guessPosition <= 8){
-            if (Number(guessedNumber) === randomGeneratedNumber){
-                console.log(chalk.green(`\nGot it! You guessed perfect.`));
-                return readline.close();
-            }
-            else if (Number(guessedNumber) > randomGeneratedNumber){
-                console.log(chalk.red(`You guessed high`));
-                return recursiveReadLine();
-            }
-            else{
-                console.log(chalk.red(`You guessed low`));
-                return recursiveReadLine();
-            }
-        }
-        else
-            return readline.close();
-    });
-}
 
 console.log('\nHello there!\nWelcome to our guessing game and following are detailed informations: ');
 console.log('- Your given eight(8) tries to guess any number between 1 and 10.');
 console.log('- Program will then tell whether you have guessed high, low or perfect.');
 console.log('- Program will exit on the fact that you have exhausted all eight(8) tries or guessed perfect.');
 
-readline.question('\nAre you ready to give it a shot?(y or n): ', (choice) => {
-    if (choice.toLowerCase().trim() === 'y'){
-        recursiveReadLine();
-    }
-    else{
-        readline.close();
-    }
-});
+let choice = input.getConsoleInput('\nAre you ready to give it a shot?(y or n): ');
 
-readline.on('close', () => {
-    if (guessPosition > 8){
-        console.log(chalk.yellow(`\nSorry! you have exhausted ALL 8 tries with incorrect guesses.Try your luck next time.`));
-        console.log(`Random Generated Number was: ${randomGeneratedNumber}`);
+if (choice.toLowerCase().trim() === 'y'){
+    let trial = 1
+    while (trial <= 8){
+        let guessedNumber = parseInt(input.getConsoleInput(`\nWhat's your ${guessingOrder[trial]} guess?: `));
+        if (guessedNumber === randomGeneratedNumber){
+            console.log(chalk.green(`\nGot it! You guessed perfect.`));
+            break;
+        }
+        else if (guessedNumber > randomGeneratedNumber){
+            console.log(chalk.red(`You guessed high`));
+        }
+        else{
+            console.log(chalk.red(`You guessed low`));
+        }
+        trial++;
     }
+    // execute when all tries exhausted
+    if (trial > 8){
+        console.log(chalk.yellow(`\nSorry! you have exhausted ALL 8 tries with incorrect guesses.Try your luck next time.`));
+        console.log(`Random Generated Number was: ${randomGeneratedNumber}\n`);
+    }
+    // execute when there is perfect guess
+    else
+        console.log(`THE GAME IS OVER!\n`);
+}
+else
     console.log(`THE GAME IS OVER!\n`);
-});
